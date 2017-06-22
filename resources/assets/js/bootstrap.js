4669aedee57170d1189f30d41365bbdbd2cf5eda
James Roper <vadremix@gmail.com>
2017-06-22 08:08:56 -0700

    Add moment.js for time formatting

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -14,6 +14,11 @@ try {
 } catch (e) {}
 
 /**
+ * Require moment.js for time formatting
+ */
+window.moment = require('moment');
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
